8e62d717355537cd757b57d8614b0da05a6dfd7e
Loris Planté <loris.plante@hotmail.fr>
2023-12-08 11:38:42 +0100

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './pages/general/home-page/home-page.component';
+import { NotFoundComponent } from './pages/general/not-found/not-found.component';
+import { ContactPageComponent } from './pages/general/contact-page/contact-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'contact', component: ContactPageComponent },
+
+  //   404 always last
+  { path: '**', component: NotFoundComponent },
+];
